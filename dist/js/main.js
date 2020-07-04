9bf966f59a3ab4157a9f148cd6c96b286ae0db95
panchuk1975 <panchuk1975vadik@gmail.com>
2020-07-04 22:55:11 +0300

    menu refactor scss

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.manu-branding');
+const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 let showMenu = false;
